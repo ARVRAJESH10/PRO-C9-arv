7af81d9df862dbab4e48eb94f41fb55421192be2
ARVRAJESH10 <89141236+ARVRAJESH10@users.noreply.github.com>
2021-08-20 19:59:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box
+
+function setup() {
+  
+  createCanvas(400,400)
+  
+  var box = createSprite(200,200,30,30)
+
+  
+}
+
+function draw() 
+{
+  background("yellow");
+  drawSprites()
+  
+}
+
+
+
+
+
+
+
